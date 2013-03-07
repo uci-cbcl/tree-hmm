4d27d068d8adcf52d5a28f9ceaa3634179df8ad6
Jake Biesinger <jake.biesinger@gmail.com>
2013-03-07 11:25:49 -0800

    Add functionality for missing marks

--- histone_tree_hmm.py
+++ histone_tree_hmm.py
@@ -56,6 +56,9 @@ import time
 from math import ceil
 import cPickle as pickle
 import copy
+import re
+import time
+import random
 
 try:
     import pysam
@@ -108,28 +111,112 @@ def dependencies_for_freezing():
     from scipy.io.matlab import streams
 
 
-valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek', 
-                 'Hmec' ]  # 'Helas3' is extra
+###################### ENCODE human ######################
+#valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek',
+#                 'Hmec' ]  # 'Helas3' is extra
 #valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
 #            'H3k4me2', 'H3k4me3', 'H3k79me2', 'H3k9ac', 'H3k9me3', 'H4k20me1']
-valid_marks = ['Ctcf', 'H3k27me3', 'H3k36me3', 'H4k20me1', 'H3k4me1', 'H3k4me2', 'H3k4me3', 'H3k27ac',  
-               'H3k9ac',  'Control',]  # no H2az or H3K9me3
+#valid_marks = ['Ctcf', 'H3k27me3', 'H3k36me3', 'H4k20me1', 'H3k4me1', 'H3k4me2', 'H3k4me3', 'H3k27ac',
+#               'H3k9ac',  'Control',]  # no H2az or H3K9me3
 #valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
 #               'H3k9ac', 'H4k20me1', 'Control',]  # H3K4me1 has an issue in Hepg2-- skipping for now
-#phylogeny = {'H1hesc':'H1hesc', 'HUVEC':'H1hesc', 'Hsmm':'H1hesc',
-#             'NHLF':'H1hesc', 'Gm12878':'H1hesc', 'K562':'Gm12878',
-#             'NHEK':'Gm12878', 'Hmec':'Gm12878', 'Hepg2':'Hsmm'}
+#
+#phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
+#             'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
+#             'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
+
+###################### modENCODE fly ######################
+#valid_species = ['E0', 'E4', 'E8', 'E12', 'E16', 'E20', 'L1', 'L2', 'L3', 'Pupae', 'Adult female', 'Adult male']
+#valid_marks = ['H3K27ac', 'H3K27me3', 'H3K4me1','H3K4me3','H3K9ac','H3K9me3']
+#phylogeny = {'E4':'E0', 'E8':'E4', 'E12':'E8','E16':'E12', 'E20':'E16',
+#             'L1':'E20','L2':'L1', 'L3':'L2', 'Pupae':'L3', 'Adult female':'Pupae', 'Adult male':'Adult female'}
+#mark_avail = sp.ones((12,6), dtype = sp.int8)
+
+###################### ENCODE mouse ######################
+#valid_species = ['Progenitor', 'Ch12', 'Erythrobl', 'G1eer4e2', 'G1e', 'Megakaryo']
+#valid_marks = ['H3k04me1', 'H3k04me3', 'H3k09me3', 'H3k27me3',  'H3k36me3', 'Input']
+#phylogeny = {'Ch12':'Progenitor', 'Erythrobl':'Progenitor', 'G1eer4e2':'Progenitor','G1e':'Progenitor', 'Megakaryo':'Progenitor',
+#            'G1eer4e2':'G1e'}
+#
+#mark_avail = sp.array([[0, 0, 0, 0, 0, 0],
+#                       [1, 1, 1, 1, 1, 1],
+#                       [1, 1, 1, 1, 1, 1],
+#                       [1, 1, 1, 1, 1, 1],
+#                       [1, 1, 1, 1, 1, 1],
+#                       [1, 1, 1, 1, 1, 1]], dtype = sp.int8)
+
+##################### ENCODE mouse2 ######################
+#valid_species = ['Bmarrow', 'Ch12', 'Erythrobl', 'G1eer4e2', 'G1e', 'Megakaryo']
+#phylogeny = {'Ch12':'Bmarrow', 'Erythrobl':'Bmarrow', 'G1eer4e2':'Bmarrow','G1e':'Bmarrow', 'Megakaryo':'Bmarrow',
+#            'G1eer4e2':'G1e'}
+#valid_marks = ['H3k27ac', 'H3k04me1', 'H3k04me3', 'H3k09me3', 'H3k27me3',  'H3k36me3', 'Input']
+#mark_avail = sp.array([[1, 1, 1, 0, 0, 0, 1],
+#                       [1, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1]], dtype = sp.int8)
+
+
+#valid_species = ['Bmarrow', 'G1e', 'G1eer4e2']
+#valid_marks = ['H3k27ac','H3k04me1', 'H3k04me3', 'H3k09me3', 'H3k27me3', 'H3k36me3', 'Input']
+#phylogeny = {'G1e':'Bmarrow', 'G1eer4e2':'G1e'}
+###
+#mark_avail = sp.array([[1, 1, 1, 0, 0, 0, 1],
+#                       [0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1]], dtype = sp.int8)
+
+
+
+############### ENCODE Human with RNA-seq ################
+valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek', 'Hmec' ]
+valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
+            'H3k4me2', 'H3k4me3', 'H3k79me2', 'H3k9ac', 'H3k9me3', 'H4k20me1']
+#valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
+#               'H3k9ac', 'H4k20me1', 'Control',]  # H3K4me1 has an issue in Hepg2-- skipping for now
+valid_marks += ['CellLongnonpolya', 'CellPap', 'CellTotal'
+                'CytosolLongnonpolya', 'CytosolPap',
+                'NucleusLongnonpolya', 'NucleusPap',
+                'NucleoplasmTotal',
+                'NucleolusTotal',
+                'ChromatinTotal',]
+mark_avail = sp.zeros((len(valid_species), len(valid_marks) * 3), dtype=sp.int8)
 phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
              'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
              'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
 
 
+
+
+#valid_species = range(20)
+#valid_marks = range(20)
+#phylogeny = {i: 0 for i in range(20)}
+
+#valid_species = ['G1e', 'G1eer4e2']
+#valid_marks = ['H3k04me1', 'H3k04me3', 'H3k09me3', 'H3k27me3', 'H3k36me3', 'Input']
+#phylogeny = {'G1eer4e2':'G1e'}
+#
+#mark_avail = sp.ones((2,6), dtype = sp.int8)
+
+
+#valid_marks = ['H3k27ac','H3k04me1', 'H3k04me3', 'H3k09me3', 'H3k27me3', 'H3k36me3', 'Input']
+#mark_avail = sp.array([[0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1],
+#                       [0, 1, 1, 1, 1, 1, 1]], dtype = sp.int8)
+
+#mark_avail = sp.ones((3,6), dtype = sp.int8)
 inference_types = ['mf', 'poc', 'pot', 'clique', 'concat', 'loopy', 'gmtk', 'indep']
+
+
 def main(argv=sys.argv[1:]):
-    """run a variational bayes algorithm"""
+    """run a variational EM algorithm"""
     # parse arguments, then call convert_data or do_inference
     parser = make_parser()
     args = parser.parse_args(argv)
+    if not hasattr(args, 'mark_avail'):
+        args.mark_avail = mark_avail
+    elif isinstance(args.mark_avail, basestring):
+        args.mark_avail = sp.load(args.mark_avail)
     if args.func == do_inference:
         # allow patterns on the command line
         all_obs = []
@@ -569,7 +656,7 @@ def make_parser():
                                 help='Restrict the total number of bins (T)')
     infer_parser.add_argument('--max_species', default=None, type=int,
                                 help='Restrict the total number of species (I)')
-    infer_parser.add_argument('--pseudocount', type=float, default=1e-6,
+    infer_parser.add_argument('--pseudocount', type=float_type, default=1e-6,
                               help='pseudocount to add to each parameter matrix')
     infer_parser.add_argument('--plot_iter', type=int, default=1,
                               help='draw a plot per *plot_iter* iterations.'
@@ -596,6 +683,8 @@ def make_parser():
                               help="Whether to save the inferred marginals for hidden variables. 0 => no saving, 1 => save at end, 2 => save at each iteration. 3 => for parallel jobs, reconstruct the chromsomal Q distribution at each iteration. Default: %(default)s")
     infer_parser.add_argument('--quiet_mode', action='store_true', help="Turn off printing for this run")
     infer_parser.add_argument('--run_local', action='store_true', help="Force parallel jobs to run on the local computer, even when SGE is available")
+    infer_parser.add_argument('--mark_avail', help='npy matrix of available marks',
+                                default=mark_avail)
     infer_parser.set_defaults(func=do_inference)
     return parser
 
