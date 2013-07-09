b4ff24f2717bae48313a027b740a459295902355
Jake Biesinger <jake.biesinger@gmail.com>
2013-07-09 01:08:33 -0700

    change defaults for static values

--- treehmm/static.py
+++ treehmm/static.py
@@ -8,20 +8,21 @@ import scipy as sp
 
 
 
-
 ###################### ENCODE human ######################
-#valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek',
-#                 'Hmec' ]  # 'Helas3' is extra
-#valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
+valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek',
+                'Hmec' ]  # 'Helas3' is extra
+# valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
 #            'H3k4me2', 'H3k4me3', 'H3k79me2', 'H3k9ac', 'H3k9me3', 'H4k20me1']
-#valid_marks = ['Ctcf', 'H3k27me3', 'H3k36me3', 'H4k20me1', 'H3k4me1', 'H3k4me2', 'H3k4me3', 'H3k27ac',
-#               'H3k9ac',  'Control',]  # no H2az or H3K9me3
-#valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
+valid_marks = ['Ctcf', 'H3k27me3', 'H3k36me3', 'H4k20me1', 'H3k4me1', 'H3k4me2', 'H3k4me3', 'H3k27ac',
+              'H3k9ac',  'Control',]  # no H2az or H3K9me3
+# valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
 #               'H3k9ac', 'H4k20me1', 'Control',]  # H3K4me1 has an issue in Hepg2-- skipping for now
-#
-#phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
-#             'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
-#             'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
+
+phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
+            'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
+            'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
+mark_avail = sp.zeros((len(valid_species), len(valid_marks)), dtype=sp.int8)
+
 
 ###################### modENCODE fly ######################
 #valid_species = ['E0', 'E4', 'E8', 'E12', 'E16', 'E20', 'L1', 'L2', 'L3', 'Pupae', 'Adult female', 'Adult male']
@@ -29,6 +30,7 @@ import scipy as sp
 #phylogeny = {'E4':'E0', 'E8':'E4', 'E12':'E8','E16':'E12', 'E20':'E16',
 #             'L1':'E20','L2':'L1', 'L3':'L2', 'Pupae':'L3', 'Adult female':'Pupae', 'Adult male':'Adult female'}
 #mark_avail = sp.ones((12,6), dtype = sp.int8)
+
 
 ###################### ENCODE mouse ######################
 #valid_species = ['Progenitor', 'Ch12', 'Erythrobl', 'G1eer4e2', 'G1e', 'Megakaryo']
@@ -42,6 +44,7 @@ import scipy as sp
 #                       [1, 1, 1, 1, 1, 1],
 #                       [1, 1, 1, 1, 1, 1],
 #                       [1, 1, 1, 1, 1, 1]], dtype = sp.int8)
+
 
 ##################### ENCODE mouse2 ######################
 #valid_species = ['Bmarrow', 'Ch12', 'Erythrobl', 'G1eer4e2', 'G1e', 'Megakaryo']
@@ -66,22 +69,22 @@ import scipy as sp
 
 
 
-############### ENCODE Human with RNA-seq ################
-valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek', 'Hmec' ]
-valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
-            'H3k4me2', 'H3k4me3', 'H3k79me2', 'H3k9ac', 'H3k9me3', 'H4k20me1']
-#valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
-#               'H3k9ac', 'H4k20me1', 'Control',]  # H3K4me1 has an issue in Hepg2-- skipping for now
-valid_marks += ['CellLongnonpolya', 'CellPap', 'CellTotal'
-                'CytosolLongnonpolya', 'CytosolPap',
-                'NucleusLongnonpolya', 'NucleusPap',
-                'NucleoplasmTotal',
-                'NucleolusTotal',
-                'ChromatinTotal',]
-mark_avail = sp.zeros((len(valid_species), len(valid_marks) * 3), dtype=sp.int8)
-phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
-             'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
-             'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
+# ############### ENCODE Human with RNA-seq ################
+# valid_species = ['H1hesc', 'K562', 'Gm12878', 'Hepg2', 'Huvec', 'Hsmm', 'Nhlf', 'Nhek', 'Hmec' ]
+# valid_marks = ['Ctcf', 'H2az', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me1',
+#             'H3k4me2', 'H3k4me3', 'H3k79me2', 'H3k9ac', 'H3k9me3', 'H4k20me1']
+# #valid_marks = ['Ctcf', 'H3k27ac', 'H3k27me3', 'H3k36me3', 'H3k4me2', 'H3k4me3',
+# #               'H3k9ac', 'H4k20me1', 'Control',]  # H3K4me1 has an issue in Hepg2-- skipping for now
+# valid_marks += ['CellLongnonpolya', 'CellPap', 'CellTotal'
+#                 'CytosolLongnonpolya', 'CytosolPap',
+#                 'NucleusLongnonpolya', 'NucleusPap',
+#                 'NucleoplasmTotal',
+#                 'NucleolusTotal',
+#                 'ChromatinTotal',]
+# mark_avail = sp.zeros((len(valid_species), len(valid_marks) * 3), dtype=sp.int8)
+# phylogeny = {'H1hesc':'H1hesc', 'Huvec':'H1hesc', 'Hsmm':'H1hesc',
+#              'Nhlf':'H1hesc', 'Gm12878':'H1hesc', 'K562':'H1hesc',
+#              'Nhek':'H1hesc', 'Hmec':'H1hesc', 'Hepg2':'H1hesc'}
 
 
 
